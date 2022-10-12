0df666f5ae48b7e91520901bd15566b73ed64c2b
physcrowley <crowlda@ecolecatholique.ca>
2022-10-11 23:27:23 -0400

    added average as output to struct()

--- RunBase.java
+++ RunBase.java
@@ -13,6 +13,7 @@ public class RunBase {
         struct();
     }
 
+    /** Utilise les méthodes du module base.Recursion */
     static void recursion()
     {
         int val = 9;
@@ -22,37 +23,44 @@ public class RunBase {
         Recursion.recurse( val );
     }
 
+    /** Utilise le struct/record défini dans base.Struct */
     static void struct()
     {
         // tenter de lire le fichier de données
         String file = "./data/struct.txt";
         Scanner fileReader;
-        try{
+        try {
             fileReader = new Scanner( new File( file ) );
-        } catch (Exception e) {
+        } catch ( Exception e ) {
             System.out.println( "Erreur : fichier \"" + file + "\" pas trouvé" );
             return; // quitter en cas d'échec
         }
 
         // créer un tableau en lisant le fichier
-        Struct[] students = new Struct[ 100 ]; // un nombre suffisament grand
-        int i = 0;
-        while ( fileReader.hasNextLine() && i < 100 )
+        Struct[] students = new Struct[ 100 ]; // un tableau assez grand
+        int n = 0; // le nombre de lignes lus
+        while ( fileReader.hasNextLine() && n < 100 )
         {
-            students[ i ] = new Struct();
-            students[ i ].name = fileReader.next();
-            students[ i ].schoolYear = fileReader.nextInt();
-            students[ i ].average = fileReader.nextDouble();
-            fileReader.nextLine();
-            i++;
+            students[ n ] = new Struct(); // un nouveau Struct à l'index n
+            students[ n ].name = fileReader.next();
+            students[ n ].schoolYear = fileReader.nextInt();
+            students[ n ].average = fileReader.nextDouble();
+            // passer à la prochaine ligne
+            fileReader.nextLine(); 
+            n++;
         }
 
         // Afficher quelques données
-        System.out.println( "Noms des " + i + " élèves :" );
+        System.out.println( "Noms des " + n + " élèves :" );
         System.out.print("\n\t");
-        for ( int s = 0; s < i; s++ ) {
+        for ( int s = 0; s < n; s++ ) {
             System.out.print( students[ s ].name + " " );
         }
         System.out.println("\n");
+
+        System.out.print( "La moyenne de la classe est ");
+        double sum = 0;
+        for ( int s = 0; s < n; s++ ) sum += students[ s ].average;
+        System.out.printf( "%.1f", sum / n );
     }
 }
