163e1992e621c7cb77d67be5a03aff3d9c9a49df
physcrowley <crowlda@ecolecatholique.ca>
2022-10-11 22:24:56 -0400

    refactored into a method

--- RunBase.java
+++ RunBase.java
@@ -2,6 +2,13 @@ import base.*;
 
 public class RunBase {
     public static void main(String[] args) {
+        
+        // exemple de récursivité
+        recursion();
+    }
+
+    static void recursion()
+    {
         int val = 9;
         System.out.println("Boucle");
         Recursion.loop( val );
