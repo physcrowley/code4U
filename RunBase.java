bba0e31383f20b1f0c05612ff7cb7127d9d995ae
physcrowley <crowlda@ecolecatholique.ca>
2022-10-11 23:06:24 -0400

    added struct/record example

--- RunBase.java
+++ RunBase.java
@@ -1,10 +1,16 @@
+import java.io.File;
+import java.util.Scanner;
+
 import base.*;
 
 public class RunBase {
     public static void main(String[] args) {
         
         // exemple de récursivité
-        recursion();
+        // recursion();
+
+        // exemple d'usage d'un objet pour organiser les données
+        struct();
     }
 
     static void recursion()
@@ -14,5 +20,39 @@ public class RunBase {
         Recursion.loop( val );
         System.out.println("Recursivité");
         Recursion.recurse( val );
+    }
+
+    static void struct()
+    {
+        // tenter de lire le fichier de données
+        String file = "./data/struct.txt";
+        Scanner fileReader;
+        try{
+            fileReader = new Scanner( new File( file ) );
+        } catch (Exception e) {
+            System.out.println( "Erreur : fichier \"" + file + "\" pas trouvé" );
+            return; // quitter en cas d'échec
+        }
+
+        // créer un tableau en lisant le fichier
+        Struct[] students = new Struct[ 100 ]; // un nombre suffisament grand
+        int i = 0;
+        while ( fileReader.hasNextLine() && i < 100 )
+        {
+            students[ i ] = new Struct();
+            students[ i ].name = fileReader.next();
+            students[ i ].schoolYear = fileReader.nextInt();
+            students[ i ].average = fileReader.nextDouble();
+            fileReader.nextLine();
+            i++;
+        }
+
+        // Afficher quelques données
+        System.out.println( "Noms des " + i + " élèves :" );
+        System.out.print("\n\t");
+        for ( int j = 0; j < i; j++ ) {
+            System.out.print( students[ i ].name + " " );
+        }
+        System.out.println("\n");
     }
 }
---
+++ base/Struct.java
@@ -0,0 +1,17 @@
+package base;
+
+/**
+ * Exemple d'une classe qui ne contient que des champs de données. 
+ * Dans certains langages ce type d'objet s'appelle un "record" ou 
+ * un "struct".
+ */
+public class Struct {
+    /** Le nom de l'élève */
+    public String name;
+
+    /** L'année scolaire, de 1 à 12 */
+    public int schoolYear;
+
+    /** La moyenne académique */
+    public double average;
+}
