fdb32fff67b0a587ca3fa898f8630b204e2b5de6
physcrowley-home <crowlda@ecolecatholique.ca>
2022-10-12 23:12:12 -0400

    ajouter des exemples dans base.Methods

--- RunBase.java
+++ RunBase.java
@@ -4,6 +4,9 @@ import java.util.Scanner;
 
 import base.*;
 
+/** Classe pour tester et lancer le code dans le package {@code base} 
+ * @author physcrowley (2022-10)
+*/
 public class RunBase {
     public static void main(String[] args) {
         
@@ -11,10 +14,13 @@ public class RunBase {
         // recursion();
 
         // exemple d'usage d'un objet pour organiser les données
-        struct();
+        // struct();
+
+        // exemple de différents types de méthodes
+        methods();
     }
 
-    /** Utilise les méthodes du module base.Recursion */
+    /** Utilise les méthodes du module {@link base.Recursion} */
     static void recursion()
     {
         int val = 9;
@@ -24,7 +30,7 @@ public class RunBase {
         Recursion.recurse( val );
     }
 
-    /** Utilise le struct/record défini dans base.Struct */
+    /** Utilise le struct/record défini dans {@link base.Struct} */
     static void struct()
     {
         // tenter de lire le fichier de données
@@ -66,5 +72,14 @@ public class RunBase {
         double sum = 0;
         for ( int s = 0; s < n; s++ ) sum += students[ s ].average;
         System.out.printf( "%.1f\n", sum / n );
+    }
+
+    /** Utilise les différentes formes de méthodes dans {@link base.Methods} */
+    static void methods(){
+        Methods.greet();
+        String name = Methods.getName();
+        int points = Methods.prize( name );
+        String prize = Methods.makePrize( name, points );
+        System.out.println( prize );
     }
 }
---
+++ base/Methods.java
@@ -0,0 +1,84 @@
+package base;
+
+import java.util.Scanner;
+
+/** Classe qui montre les différents types de méthodes.
+ * <p>
+ * Toutes les méthodes ont le format [type] [identifiant]( [paramètres] ) {...}
+ * <p>
+ * Le type est liée à la valeur de retour de la méthode. Si la méthode ne retourne
+ * rien, son type est {@code void}.
+ * @author physcrowley (2022-10)
+ */
+public class Methods {
+
+    private static Scanner input = new Scanner( System.console().reader() );
+
+    /** Méthode sans paramètre ni valeur de retour */
+    public static void greet(){
+        System.out.println( "Bonjour!" );
+    }
+
+    /** Méthode sans paramètre qui retourne une valeur
+     * @return le nom spécifié par l'utilisateur au clavier
+     */
+    public static String getName(){
+        System.out.print( "Quel est votre nom? > " );
+        return input.nextLine();
+    }
+
+    /** Méthode avec paramètre et avec valeur de retour 
+     * @param s n'importe quel String
+     * @return une valeur entière selon la longueur du String {@code s}
+    */
+    public static int prize( String s ) {
+        int n = s.length();
+        if ( n >= 10 ) return 10;
+        if ( n >= 5 ) return 5;
+        return 1;
+
+        /*
+        * Notez que l'instruction `return` met fin immédiatement à la méthode.
+        * 
+        * C'est pourquoi ce serait inutile de mettre des `else` si le bloc `if`
+        * contient une instruction de retour. 
+        *
+        * Si la condition précédante est true, le `return` met fin à la méthode. 
+        * Sinon, on passe à la ligne suivante. Donc on passe seulement à la 
+        * ligne suivante si le `if` est false, exactement comme la structure 
+        * if - else if.
+        * 
+        */
+    }
+
+    /**
+     * Une méthode avec plusieurs paramètres et une valeur de retour
+     * 
+     * @param name le nom du/de la gagnant/e
+     * @param points le nombre de points obtenus
+     * @return un String représentant une pyramide de taille proportionnelle
+     * au nombre de points
+     */
+    public static String makePrize( String name, int points ){
+        StringBuilder out = new StringBuilder(); 
+        out.append( "\n" + name + ", votre nom vous mérite cette pyramide :\n\n" );
+        // triangle grandissant
+        for ( int i = 1; i <= points; i++ ){
+            for ( int j = 0; j < i; j++ ){
+                out.append( "O" );
+            }
+            out.append( "\n" ); // fin d'une ligne
+        }
+        // triangle diminuant
+        for ( int i = points - 1; i > 0; i-- ){
+            for ( int j = 0; j < i; j++ ){
+                out.append( "O" );
+            }
+            out.append( "\n" ); // fin d'une ligne
+        }
+        return out.toString();
+    }
+
+    
+    
+}
