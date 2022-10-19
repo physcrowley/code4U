204ea11d69d87bc80804c1e1f36cc32ce00871e3
physcrowley <crowlda@ecolecatholique.ca>
2022-10-19 01:36:59 -0400

    added method for Tableaux examples

--- base/Run.java
+++ base/Run.java
@@ -11,7 +11,10 @@ public class Run {
     public static void main(String[] args) {
         
         // exemple de différents types de méthodes
-        methods();
+        // methods();
+
+        // exemples de traverse de tableaux
+        traversingArrays();
         
         // exemple de récursivité
         // recursion();
@@ -33,14 +36,48 @@ public class Run {
         System.out.println( prize );
     }
 
+    /** 
+     * Utilise les méthodes pour traverser les tableaux définis 
+     * dans {@link base.Tableaux}
+     *  */
+    static void traversingArrays() {
+        System.out.println( "Traverser les tableaux" );
+        System.out.println( "\nWHILE");
+        Tableaux.traverseWithWhile();
+        System.out.println( "\nFOR" );
+        Tableaux.traverseWithFor();
+        System.out.println( "\nFOR EACH" );
+        Tableaux.traverseWithForEach();
+        System.out.println();
+    }
+
     /** Utilise les méthodes du module {@link base.Recursion} */
     static void recursion()
     {
-        int val = 9;
-        System.out.println("Boucle");
-        Recursion.loop( val );
-        System.out.println("Recursivité");
-        Recursion.recurse( val );
+        int[] nums = {-7, 0, 1, 2, 3, 20, 40};
+        
+        System.out.println( "Factoriel " );
+        for ( int n : nums ){
+            long result = Recursion.factorial( n );
+            String err = "";
+            if ( result == -1 ) err = "erreur de nombre négatif";
+            else if ( result < 0 ) err = "erreur de résultat trop grand";
+            System.out.printf( "%6d -> %d %s\n", n, result, err );
+        }
+        System.out.println( "\nOreils de lapin" );
+        //ne pas faire le cas négatif
+        for ( int i = 1; i < nums.length; i++ ) {
+            int n = nums[ i ]; 
+            System.out.printf(
+                "%6d lapins -> %d oreils\n", n, Recursion.bunnyEars( n ) );
+        }
+        System.out.println( "\nFibonacci" );
+        for ( int n : nums ){
+            int result = Recursion.fibonacci( n );
+            String err = result == -1 ? "erreur de nombre négatif" : "";
+            System.out.printf(
+                "    n=%2d -> %d %s\n", n, result, err );
+        }
     }
 
     /** Utilise le struct/record défini dans {@link base.Struct} */
