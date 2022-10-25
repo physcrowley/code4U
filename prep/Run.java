0aa7c0bbaf43678b9b410ae122ec7ded35f40cec
physcrowley <crowlda@ecolecatholique.ca>
2022-10-25 01:25:24 -0400

    refactored

--- prep/Run.java
+++ prep/Run.java
@@ -1,42 +1,41 @@
 package prep;
 
 import java.io.File;
-import java.util.Locale;
 import java.util.Scanner;
 
-/** Classe pour tester et lancer le code dans le package {@code base} 
+/** Classe pour tester et lancer le code dans le package {@code prep} 
  * @author physcrowley (2022-10)
 */
 public class Run {
+
+    /** 
+     * Utilise la structure de données définie dans le struct/record
+     *  {@link prep.Struct} 
+     *  et les méthodes définies dans le module {@link prep.Module}
+     */
     public static void main(String[] args) {
         
-        // exemple d'usage d'un objet pour organiser les données
-        struct();
-    }
+        String file = "./data/struct.txt"; // chemin du fichier de données à lire
+        Scanner fileReader; // un Scanner pour le fichier
 
-    
-    /** Utilise le struct/record défini dans {@link base.Struct} */
-    static void struct()
-    {
-        // tenter de lire le fichier de données
-        String file = "./data/base_struct.txt"; 
-                      // chemin à partir de la racine du "projet" code4U
-        Scanner fileReader;
-        try {
+        // On utilise try/catch pour gérer l'erreur potentielle d'absence de 
+        // fichier sans faire planter le programme
+        try { 
             fileReader = new Scanner( new File( file ) );
-            fileReader.useLocale( Locale.CANADA ); // le décimal est un point (.)
         } catch ( Exception e ) {
-            System.out.println( "Erreur : fichier \"" + file + "\" pas trouvé" );
+            System.err.println( "Erreur : fichier \"" + file + "\" pas trouvé" );
             return; // quitter en cas d'échec
         }
 
-        // créer un tableau en lisant le fichier
+        // En cas de réussite, créer un tableau en lisant le fichier
         Struct[] students = new Struct[ 100 ]; // un tableau assez grand
+
+        // Lire le fichier et assigner les valeurs à nos Struct
         int n = 0; // le nombre de lignes lus
         while ( fileReader.hasNextLine() && n < 100 )
         {
             students[ n ] = new Struct(); // un nouveau Struct à l'index n
-            // extraires les valeurs pour cet élève
+            // extraire les valeurs pour cet élève
             students[ n ].name = fileReader.next();
             students[ n ].schoolYear = fileReader.nextInt();
             students[ n ].average = fileReader.nextDouble();
@@ -44,20 +43,13 @@ public class Run {
             fileReader.nextLine(); 
             n++;
         }
+        
+        // Fermer le Scanner du fichier pour protéger son contenu
         fileReader.close();
 
         // Afficher quelques données
-        System.out.println( "Noms des " + n + " élèves :" );
-        System.out.print("\n\t");
-        for ( int s = 0; s < n; s++ ) {
-            System.out.print( students[ s ].name + " " );
-        }
-        System.out.println("\n");
-
-        System.out.print( "La moyenne de la classe est ");
-        double sum = 0;
-        for ( int s = 0; s < n; s++ ) sum += students[ s ].average;
-        System.out.printf( "%.1f\n", sum / n );
+        Module.showList( n, students );
+        Module.showAverage( n, students );
     }
 
 }
