b7d8fe90bb9d4456cd25893c5133dad44f584296
physcrowley <crowlda@ecolecatholique.ca>
2022-11-18 10:44:28 -0500

    added NextLine example

---
+++ base/NextLine.java
@@ -0,0 +1,28 @@
+import java.util.Locale;
+import java.util.Scanner;
+
+public class NextLine {
+    public static void main(String[] args) {
+        Scanner console = new Scanner( System.console().reader() );
+
+        System.out.println( "Entrer des pourcentages (avec .) séparées par des espaces.");
+        System.out.println( "Taper [Entrée] pour terminer.");
+
+        double sum = 0, count = 0;
+        String line = console.nextLine();
+
+        Scanner input = new Scanner( line ); // scanner le String
+        input.useLocale(Locale.CANADA); // valeurs au format ##.#
+        while ( input.hasNext() )
+        {
+            sum += input.nextDouble();
+            count++;
+        }
+        input.close();
+        if ( count > 0 )
+        {
+            System.out.println( "La moyenne est : " + sum / count);
+        }
+    }
+    
+}
