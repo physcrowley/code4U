a9e96cb233d866faa9a9be3a507a68c63d38d14b
physcrowley <crowlda@ecolecatholique.ca>
2022-12-06 23:38:38 -0500

    update example

--- Static.java
+++ Static.java
@@ -1,21 +1,23 @@
 public class Static {
     public static void main(String[] args) {
         Obj a = new Obj("A");
-        System.out.println( a.name + " " + a.count );
+        System.out.println( a.name + ", " + a.id + " de " + Obj.count );
+        
         Obj b = new Obj("B");
-        System.out.println( b.name + " " + b.count );
-        System.out.println( a.name + " " + a.count );
-        System.out.println( "Obj "  + Obj.count );
+        System.out.println( b.name + ", " + b.id + " de " + Obj.count );
+        System.out.println( a.name + ", " + a.id + " de " + Obj.count );
     }
 }
 
 class Obj{
     static int count = 0; // appartient à la classe
     String name;
+    int id;
 
     Obj( String s )
     {
         count++;
         this.name = s;
+        this.id = count;
     }
 }
